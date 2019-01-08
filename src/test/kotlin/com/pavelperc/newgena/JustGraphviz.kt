44082184ff3a4a7ca6e70127c15280b7e57a007b
pavelperc <pavelperc@mail.ru>
2019-01-08 16:42:14 +0300

    Some changes in graphviz examples.

--- src/test/kotlin/com/pavelperc/newgena/JustGraphviz.kt
+++ src/test/kotlin/com/pavelperc/newgena/JustGraphviz.kt
@@ -1,10 +1,7 @@
 package com.pavelperc.newgena
 
 import guru.nidi.graphviz.*
-import guru.nidi.graphviz.attribute.Arrow
-import guru.nidi.graphviz.attribute.Color
-import guru.nidi.graphviz.attribute.RankDir
-import guru.nidi.graphviz.attribute.Style
+import guru.nidi.graphviz.attribute.*
 import guru.nidi.graphviz.engine.Format
 import guru.nidi.graphviz.engine.Graphviz
 import guru.nidi.graphviz.model.Compass
@@ -18,38 +15,64 @@ class JustGraphviz {
     
     
     @Test
-    fun javaStyle() {
+    fun immutableStyle() {
         val g = graph("example1").directed()
                 .graphAttr().with(RankDir.LEFT_TO_RIGHT)
                 .with(
                         node("a").with(Color.RED).link(node("b")),
                         node("b").link(Factory.to(node("c")).with(Style.DASHED))
                 )
-        Graphviz.fromGraph(g).height(100).render(Format.SVG).toFile(File("gv/java_style.svg"))
+        Graphviz.fromGraph(g).height(100).render(Format.SVG).toFile(File("gv/immutable_simple.svg"))
     }
     
     @Test
-    fun kotlin2() {
-        graph(directed = true) {
+    fun immutableStyleComplex() {
+        val main = node("main").with(Label.html("<b>main</b><br/>start"), Color.rgb("1020d0").font())
+        val init = node(Label.of("**_init_**"))
+        val execute = node("execute")
+        val compare = node("compare").with(Shape.RECTANGLE, Style.FILLED, Color.hsv(.7, .3, 1.0))
+        val mkString = node("mkString").with(Label.of("make a\nstring"))
+        val printf = node("printf")
+        
+        val g = graph("example2").directed().with(
+                main.link(
+                        Factory.to(node("parse").link(execute)), //.with(LinkAttr.weight(8)),
+                        Factory.to(init).with(Style.DOTTED),
+                        Factory.node("cleanup"),
+                        Factory.to(printf).with(Style.BOLD, Label.of("100 times"), Color.RED)),
+                execute.link(
+                        graph().with(mkString, printf),
+                        Factory.to(compare).with(Color.RED)),
+                init.link(mkString))
+    
+        Graphviz.fromGraph(g).width(900).render(Format.SVG).toFile(File("gv/immutable_complex.svg"))
+    }
+    
+    @Test
+    fun kotlinStyle1() {
+        graph(directed = true, name = "example") {
+            edge["color" eq "red", Arrow.TEE]
+            node[Color.GREEN]
+            graph[RankDir.LEFT_TO_RIGHT, "label" eq "example graph"]
+            
+            
+            "a" - "b" - "c"
+            ("c"[Color.RED] - "d"[Color.BLUE])[Arrow.VEE]
+            "d" / Compass.NORTH - "e" / Compass.SOUTH
+        }.toGraphviz().render(Format.SVG).toFile(File("gv/kotlin_style.svg"))
+    }
+    
+    @Test
+    fun kotlinStyle2() {
+        val graphviz = graph(directed = true) {
             graph[RankDir.LEFT_TO_RIGHT]
             
             "a"[Color.RED] - ("b" - "c")[Style.DASHED]
             
             
             mutNode("d")
-        }.toGraphviz().render(Format.SVG).toFile(File("gv/kotlin2.svg"))
-    }
-    
-    @Test
-    fun kotlinStyleSimple() {
-        graph(directed = true, name = "example") {
-            edge["color" eq "red", Arrow.TEE]
-            node[Color.GREEN]
-            graph[RankDir.LEFT_TO_RIGHT]
-
-            "a" - "b" - "c"
-            ("c"[Color.RED] - "d"[Color.BLUE])[Arrow.VEE]
-            "d" / Compass.NORTH - "e" / Compass.SOUTH
-        }.toGraphviz().render(Format.SVG).toFile(File("gv/kotlin_style.svg"))
+        }.toGraphviz()
+        
+        graphviz.render(Format.SVG).toFile(File("gv/kotlin2.svg"))
     }
 }
