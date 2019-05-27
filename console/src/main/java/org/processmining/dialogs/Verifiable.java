3f2aaeb36917e125f5bbac7a053f4c1d65207cd5
pavelperc <pavelperc@mail.ru>
2019-05-27 21:45:23 +0300

    Removed all old prom interface files. Cleared useless (I hope) packages (see YaDisk).

--- console/src/main/java/org/processmining/dialogs/Verifiable.java
+++
@@ -1,9 +0,0 @@
-package org.processmining.dialogs;
-
-/**
- * Created by Ivan on 10.03.2016.
- */
-public interface Verifiable
-{
-    boolean verify();
-}
--- console/src/main/kotlin/com/pavelperc/newgena/loaders/pnml/PnmlLoader.kt
+++ console/src/main/kotlin/com/pavelperc/newgena/loaders/pnml/PnmlLoader.kt
@@ -1,17 +1,9 @@
 package com.pavelperc.newgena.loaders.pnml
 
-import com.pavelperc.newgena.models.makePnmlIdsFromLabels
-import com.pavelperc.newgena.models.makePnmlIdsOrdinal
-import org.processmining.models.connections.GraphLayoutConnection
-import org.processmining.models.graphbased.directed.petrinet.ResetInhibitorNet
-import org.processmining.models.graphbased.directed.petrinet.impl.PetrinetFactory
-import org.processmining.models.graphbased.directed.petrinet.impl.ResetInhibitorNetImpl
-import org.processmining.models.semantics.petrinet.Marking
 import org.processmining.plugins.pnml.Pnml
 import org.xmlpull.v1.XmlPullParserFactory
 import java.io.File
 import java.io.FileInputStream
-import java.lang.Exception
 
 
 object PnmlLoader {
@@ -53,21 +45,21 @@ object PnmlLoader {
     fun loadPetrinetWithOwnParser(path: String) = PnmlOwnParser.parseFromFile(File(path))
     
     
-    @Deprecated("Use loadPetrinetWithOwnParser instead.")
-    fun loadPetrinet(
-            path: String
-    ): Pair<ResetInhibitorNet, Marking> {
-        val pnml = loadPnml(path)
-        
-        val petrinet = PetrinetFactory.newResetInhibitorNet(pnml.label)!!
-        petrinet.makePnmlIdsFromLabels()
-        petrinet.edges.toList().makePnmlIdsOrdinal()
-        
-        val marking = Marking()
-        
-        pnml.convertToNet(petrinet, marking, GraphLayoutConnection(petrinet))
-        
-        return petrinet to marking
-    }
+//    @Deprecated("Use loadPetrinetWithOwnParser instead.")
+//    fun loadPetrinet(
+//            path: String
+//    ): Pair<ResetInhibitorNet, Marking> {
+//        val pnml = loadPnml(path)
+//        
+//        val petrinet = PetrinetFactory.newResetInhibitorNet(pnml.label)!!
+//        petrinet.makePnmlIdsFromLabels()
+//        petrinet.edges.toList().makePnmlIdsOrdinal()
+//        
+//        val marking = Marking()
+//        
+//        pnml.convertToNet(petrinet, marking, GraphLayoutConnection(petrinet))
+//        
+//        return petrinet to marking
+//    }
 }
 
